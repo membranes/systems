5d811386ba17df1f951f31abb77969732c295502
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-23 23:47:51 +0100

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,52 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Module conf.py
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+Notes
+------
 
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+Project information:
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+"""
+import os
+import sys
+import datetime
+import revitron_sphinx_theme
+
+
+'''
+Path
+'''
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../revitron'))
+
+
+'''
+Master
+'''
+master_doc = 'index'
+
+
+'''
+Basic
+'''
 project = 'Systems'
-copyright = '2024, greyhypotheses'
+project_copyright = '{}, <a href="https://github.com/greyhypotheses">greyhypotheses</a>'.format(datetime.datetime.now().year)
 author = 'greyhypotheses'
-release = '0.2.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+'''
+General configuration
+https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+Sphinx extension modules. Extensions are either Sphinx extensions, named 'sphinx.ext.*', 
+or custom ones.  Note, myst_enable_extensions excludes 'linkify'
+'''
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
