bfd043332d0f4979f7eed14589cf989257ad6cf5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-23 23:55:24 +0100

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,28 @@ extensions = [
     'sphinxext.opengraph',
 ]
 
+add_module_names = False
+
+napoleon_google_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+
+
+
+
+
+
+
+
+
 templates_path = ['_templates']
 exclude_patterns = []
 
